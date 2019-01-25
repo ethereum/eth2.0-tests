606ecf1a36780e5ae517d80e7217953197e3dc9a
Chih Cheng Liang <chihchengliang@gmail.com>
2019-01-25 17:25:18 +0800

    remove unused typing

--- eth2_testgen/bls/eth2_typing.py
+++ eth2_testgen/bls/eth2_typing.py
@@ -1,26 +1,7 @@
 from typing import (
-    NamedTuple,
     NewType,
 )
 
 
-SlotNumber = NewType('SlotNumber', int)  # uint64
-ShardNumber = NewType('ShardNumber', int)  # uint64
 BLSPubkey = NewType('BLSPubkey', bytes)  # bytes48
 BLSSignature = NewType('BLSSignature', bytes)  # bytes96
-
-Bitfield = NewType('Bitfield', bytes)  # uint64
-
-
-ValidatorIndex = NewType('ValidatorIndex', int)  # uint24
-CommitteeIndex = NewType('CommitteeIndex', int)
-
-Ether = NewType('Ether', int)  # uint64
-Gwei = NewType('Gwei', int)  # uint64
-
-Timestamp = NewType('Timestamp', int)
-Second = NewType('Second', int)
-
-
-class FromBlockParams(NamedTuple):
-    slot: SlotNumber = None
